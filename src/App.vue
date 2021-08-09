d97d21f374c61487eded216560642ed766c3665f
insungcho1939 <insungcho1939@gmail.com>
2021-08-09 19:27:24 +0900

    vue seminar example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+    props example(parent) : {{ propsData }}
+    <button @click="addProps">props</button>
+    <br />
+
+    state example : {{ $store.state.count }} getter example :
+    {{ gettersParent }}
+    <button @click="addCount">+</button>
+    <button @click="subCount">-</button> <br />
+
+    mutation,actions example : {{ $store.state.integer }}
+    <button @click="mutations">x3</button>
+    <button @click="actions">after 3s</button>
+    <br />
+
+    <!-- <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button> -->
+
+    <Child :props="propsData" v-on:subProps="substraction" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header.vue";
+import Child from "./Child.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex"; // mapActions 넣어주기, , mapMutations
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header,
+    Child
+  },
+  data() {
+    return {
+      propsData: 100
+    };
+  },
+  computed: mapGetters({
+    gettersParent: "gettersCount"
+  }),
+  methods: {
+    ...mapMutations({ mutations: "mutationsTriple" }),
+    ...mapActions({ actions: "actionsTime" }),
+    // mutations() {
+    //   this.$store.commit("mutationsTriple");
+    // },
+    //  actions() {
+    //   this.$store.dispatch("actionsTime");
+    // },
+    addProps() {
+      this.propsData++;
+    },
+    substraction() {
+      this.propsData--;
+    },
+    addCount() {
+      this.$store.state.count++;
+    },
+    subCount() {
+      this.$store.state.count--;
+    }
+
+    // getters() {
+    //   this.$store.dispatch("gettersCount");
+    // }
+
+    // addCounter() {
+    //   this.$store.state.count++;
+    //   this.$store.dispatch("addCounter");
+    // },
+    // subCounter() {
+    //   // this.counter--;
+    //   this.$store.state.count--;
+    //   this.$store.state.counter--; //data속성으로 선언한 것들 제외함 그리고 하위컴포넌트로 데이터 전달 x
+    // }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <hr />
+    <!-- Child counter: {{ $store.state.counter }} <br /> -->
+    props example(child): {{ props }} <button @click="emitBtn">emit</button>
+    <br />
+    getter exmaple: {{ gettersChild }} <br />
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  props: ["props"],
+  computed: mapGetters({
+    gettersChild: "gettersCount"
+  }),
+  methods: {
+    emitBtn() {
+      this.$emit("subProps");
+    }
+  }
+};
+</script>
